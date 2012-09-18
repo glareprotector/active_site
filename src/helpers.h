c7eea56e107a39531e2fcd2634203753cf88ad8a
fulton <fultonw@dragon.csail.mit.edu>
2012-09-18 15:30:40 -0400

    forgot to add helper.h after changing to lite_array

--- src/helpers.h
+++ src/helpers.h
@@ -4,32 +4,27 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include "lite_fixed.hpp"
 
 namespace helpers{
-  
-  // a and b can be 0.  
-  string split_string(string s, char delim, int a, int b){
-    // find the position of nth occurent of delim from the end
-    assert(a < b);
-    int len = s.size();
-    int count = 0;
-    int a_pos = len, b_pos = 0;
-    for(int i = len-1; i >= 0; i--){
-      if(s[i] == delim){
-	if(count == a){
-	  a_pos = i;
-	}
-	if(count == b){
-	  b_pos = i+1;
-	}
-	count++;
-      }
+
+  template<typename T>
+    void append(lite::array<T[1]>& ar, T x){
+
+    lite::array<T[1]> copy(ar);
+    int the_size = ar.size().i0;
+    ar.resize(the_size + 1);
+    for(int i = 0; i < the_size; i++){
+      ar(i) = copy(i);
     }
-    a_pos = max(a_pos, 0);
-    b_pos = min(b_pos, len-1);
-    assert(b_pos < a_pos);
-    return s.substr(b_pos, a_pos - b_pos);
+    ar(the_size) = x;
+    
+
   }
+  
+
+
+
 
 }
 
