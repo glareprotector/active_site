c4164d35caa6226ff4fdb605eba60a4c91d5967f
Fulton Wang <fw27@clarinet000-207.orchestra>
2012-09-24 19:24:26 -0400

    asdf

--- src/tester.py
+++ src/tester.py
@@ -7,8 +7,15 @@ import global_stuff
 
 # hardcode parameters for the experiment here for now.
 
-the_dict = {'pdb_name':'1asy', 'chain_letter':'A', 'edge_feature_list':[features.xW], 'node_feature_list':[features.vW, features.uW, features.wW], 'dist_cut_off':5}
+#the_dict = {'pdb_name':'1asy', 'chain_letter':'A', 'edge_feature_list':[features.xW], 'node_feature_list':[features.vW, features.uW, features.wW], 'dist_cut_off':5}
 
-the_params = param.param(the_dict)
+#the_params = param.param(the_dict)
+from parameters import the_params
 pdb.set_trace()
-print wc.get_stuff(objects.jW, the_params, global_stuff.recalculate, True, True)
+ans = wc.get_stuff(objects.bmW, the_params, global_stuff.recalculate, False, False)
+
+asdf = [ans[j][0] for j in range(len(ans))]
+sum = 0
+for i in asdf:
+    sum += i*i
+print sum / len(ans)
