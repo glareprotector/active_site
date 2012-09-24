e86e98b6fa23b1599693bc2e90629bbfcf0388fc
fulton <fultonw@dragon.csail.mit.edu>
2012-09-23 21:50:50 -0400

    add generate old input files

---
+++ src/generate_old_input_files.py
@@ -0,0 +1,59 @@
+import pdb
+import wc
+import new_new_objects as objects
+import param
+import global_stuff
+import wrapper
+import os
+from wrapper_decorator import dec
+import f as features
+
+base_folder = '/mnt/work/fultonw/active_site/active_site/test/'
+
+class generate_old_input_files(wrapper.obj_wrapper):
+
+    @dec
+    def constructor(self, params, recalculate = False, to_pickle = False, to_filelize = False, always_recalculate = False):
+        pdb_names = self.get_param(params, 'pdb_names')
+        chain_letters = self.get_param(params, 'chain_letters')
+        for i in range(len(pdb_names)):
+
+            pdb_name = pdb_names[i]
+            chain_letter = chain_letters[i]
+            self.set_param(params, 'pdb_name', pdb_name)
+            self.set_param(params, 'chain_letter', chain_letter)
+
+            the_folder = base_folder + pdb_name + '/'
+
+            if not os.path.exists(the_folder):
+                os.makedirs(the_folder)
+
+            node_features = self.get_var_or_file(objects.jW, params, False, False, False)
+            transposed_node_features = global_stuff.get_transpose(node_features)
+            pdb.set_trace()
+            global_stuff.write_mat(transposed_node_features, the_folder + 'Xnode.csv')
+            pdb.set_trace()
+            edge_features = self.get_var_or_file(objects.kW, params, False, False, False)
+            edge_features_transposed = global_stuff.get_transpose(edge_features)
+            global_stuff.write_mat(edge_features_transposed, the_folder + 'Xedge.csv')
+
+            true_states = self.get_var_or_file(objects.oW, params, False, False, False)
+            global_stuff.write_vect(true_states, the_folder + 'true_y.csv')
+
+            edge_list = self.get_var_or_file(objects.iW, params, False, False, False)
+            global_stuff.write_mat(edge_list, the_folder + 'edge_list.csv')
+
+            info = [str(len(node_features)), str(len(edge_features))]
+            global_stuff.write_vect(info, the_folder + 'info.txt', the_sep = ' ')
+            
+            
+        return None
+
+#pdb_names = ['12as','2jcw','13pk','1a4i','1a4s','1ab8']
+pdb_names = ['2jcw']
+#chain_letters = ['A','A','A','A','A','A']
+chain_letters = ['A']
+
+the_params = param.param({'pdb_names':pdb_names, 'chain_letters':chain_letters, 'edge_feature_list':[features.xW], 'node_feature_list':[features.vW, features.uW, features.wW, features.zW], 'dist_cut_off':5})
+
+wc.get_stuff(generate_old_input_files,the_params, False, False, False, False)
