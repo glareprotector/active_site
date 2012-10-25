3d1a454913bb4c6817dde173644c6026768c6c39
fulton <fultonw@dragon.csail.mit.edu>
2012-10-24 23:06:47 -0400

    fogot to add tester

--- src/tester.py
+++ src/tester.py
@@ -10,12 +10,20 @@ import global_stuff
 #the_dict = {'pdb_name':'1asy', 'chain_letter':'A', 'edge_feature_list':[features.xW], 'node_feature_list':[features.vW, features.uW, features.wW], 'dist_cut_off':5}
 
 #the_params = param.param(the_dict)
-from parameters import the_params
-pdb.set_trace()
-ans = wc.get_stuff(objects.bmW, the_params, global_stuff.recalculate, False, False)
 
-asdf = [ans[j][0] for j in range(len(ans))]
-sum = 0
-for i in asdf:
-    sum += i*i
-print sum / len(ans)
+
+import helper
+
+file_location = 'logreg'
+
+folder_name, the_params = helper.read_param(file_location)
+
+the_params.set_param('p', '9pap')
+the_params.set_param('c', 'A')
+the_params.set_param('st', 1)
+the_params.set_param('en', 212)
+
+
+ans = wc.get_stuff(objects.ciW, the_params, True, False, False)
+
+print ans
